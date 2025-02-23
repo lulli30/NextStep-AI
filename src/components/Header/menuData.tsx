fbecd18993286b4c1c39be08f97854d30075be7b
lulli <johnandrewborabo44@gmail.comm>
2025-02-23 22:54:38 +0800

    Removed about page

--- src/components/Header/menuData.tsx
+++ src/components/Header/menuData.tsx
@@ -7,11 +7,5 @@ const menuData: Menu[] = [
     path: "/",
     newTab: false,
   },
-  {
-    id: 2,
-    title: "About",
-    path: "/about",
-    newTab: false,
-  },
 ];
 export default menuData;
