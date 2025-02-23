1395c9b722c1eb45d4f00d4f0ac2dfc2d8210a4f
lulli <johnandrewborabo44@gmail.comm>
2025-02-24 00:24:19 +0800

    removed the hidden class entirely, and I replaced md:block with block.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -139,13 +139,13 @@ const Header = () => {
                 <>
                   <Link
                     href="/signin"
-                    className="hidden px-7 py-3 text-base font-medium text-dark hover:opacity-70 dark:text-white md:block"
+                    className="px-7 py-3 text-base font-medium text-dark hover:opacity-70 dark:text-white md:block"
                   >
                     Sign In
                   </Link>
                   <Link
                     href="/signup"
-                    className="hidden rounded-sm bg-primary px-8 py-3 text-base font-medium text-white shadow-btn transition duration-300 hover:bg-opacity-90 hover:shadow-btn-hover md:block md:px-9 lg:px-6 xl:px-9"
+                    className="rounded-sm bg-primary px-8 py-3 text-base font-medium text-white shadow-btn transition duration-300 hover:bg-opacity-90 hover:shadow-btn-hover md:block md:px-9 lg:px-6 xl:px-9"
                   >
                     Sign Up
                   </Link>
--- src/components/Header/menuData.tsx
+++ src/components/Header/menuData.tsx
@@ -7,5 +7,17 @@ const menuData: Menu[] = [
     path: "/",
     newTab: false,
   },
+  {
+    id: 2,
+    title: "Login",
+    path: "/login",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "Register",
+    path: "/register",
+    newTab: false,
+  },
 ];
 export default menuData;
